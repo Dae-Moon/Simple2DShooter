9788b817423ea5378c3316e9c04d574c17454431
DaeMoon <myakinin-dmitriy77@mail.ru>
2025-02-14 07:33:34 +0300

    WeaponType removal and refactoring of the Shoot method
    Removed the enumerated type `WeaponType`, replaced by a protected virtual `OnShoot` method to implement shooting behavior in subclasses. The `Shoot` method now checks for reloading and calls `OnShoot` instead of returning a value. Removed specific implementations of firing behavior for different weapon types, improving code flexibility.

--- Weapon.cs
+++ Weapon.cs
@@ -1,17 +1,9 @@
 ﻿namespace Simple2DShooter;
 
-public enum WeaponType
-{
-    Knife,
-    Gun,
-    Shotgun
-}
-
 internal class Weapon
 {
     private static DateTime _lastShootTime = DateTime.Now;
 
-    public WeaponType weaponType;
     public float cooldown;
     public float knockback;
     public float bulletSpeed;
@@ -19,43 +11,15 @@ internal class Weapon
     public float damageDistance;
     public int damage, killCount;
 
+    protected virtual void OnShoot(Vector2 position) { }
+
     public void Shoot(Vector2 position)
-    {
-        if (!CooldownPassed())
-            return;
-
-        if (weaponType == WeaponType.Knife)
-        {
-            var pos = Input.MousePosition.ToVector();
-            var dir = Utils.random.NextSingle(0f, 360f).ToDirection() * 15f;
-            new KnifeEffect(pos + dir, pos - dir, this);
-        }
-        else if (weaponType == WeaponType.Shotgun)
-        {
-            var angle = (Input.MousePosition.ToVector() - position).ToAngle();
-
-            for (int i = 0; i < 4; i++)
-                new Bullet(position, Utils.random.NextSingle(angle - 2.5f, angle + 2.5f).ToDirection(), this);
-            
-            for (int i = 0; i < 3; i++)
-                new Bullet(position, Utils.random.NextSingle(angle - 15f, angle + 15f).ToDirection(), this);
-
-            for (int i = 0; i < 2; i++)
-                new Bullet(position, Utils.random.NextSingle(angle - 35f, angle + 35f).ToDirection(), this);
-        }
-        else
-            new Bullet(position, Input.MousePosition.ToVector() - position, this);
-    }
-
-    private bool CooldownPassed()
     {
         var elapsedTime = (DateTime.Now - _lastShootTime).TotalSeconds;
         if (elapsedTime >= cooldown)
         {
             _lastShootTime = DateTime.Now;
-            return true;
+            OnShoot(position);
         }
-
-        return false;
     }
 }
